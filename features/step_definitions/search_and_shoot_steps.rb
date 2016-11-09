84662a0047a39049754a7aa8c790df33996ff670
Vitaly Pestov <ariekdev@gmail.com>
2016-11-09 14:30:53 +0200

    initial google search with site_prism

--- features/step_definitions/search_and_shoot_steps.rb
+++ features/step_definitions/search_and_shoot_steps.rb
@@ -1,14 +1,25 @@
+class GoogleSearch < SitePrism::Page
+  set_url 'http://www.google.com'
+
+  element :search_field, "input[name='q']"
+  element :search_button, "button[name='btnG']"
+end
+
 Given(/^I am on google search page$/) do
+  @google = GoogleSearch.new
+  @google.load
 end
 
 # Scenario 1:
 
 When(/^I enter search subject$/) do
-
+  expect(@google).to have_search_field
+  @google.search_field.set "Aikido"
+  @google.search_button.click
 end
 
 Then(/^I should find wiki link in search results$/) do
-
+  sleep 1
 end
 
 When(/^I navigate wiki link$/) do
